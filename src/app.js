6675c436eb5fde49e6a8be4444c3cfcfa56a87df
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-08-13 20:19:14 -0300

    Criação da requisição do post de livros

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json()); // Isto é um Middleware --> utilizado para ter acesso a requisições e adicionar algumas coisas nelas
 
 const livros = [
     {
@@ -10,15 +11,31 @@ const livros = [
     {
         id: 2,
         titulo: "O Hobbit"    
-    }
-]
+    },
+];
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+}
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.JS");
 });
 
 app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
-})
+    res.status(200).json(livros); // 200 --> OK
+});
+
+app.get("/livros/:id", (req, res) => {
+
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso"); // 201 --> Registro criado
+});
+
 
 export default app;
